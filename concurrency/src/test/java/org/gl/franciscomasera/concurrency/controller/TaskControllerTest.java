cda4220209b0274208f805cb608d430a6420a36e
Dargor94 <franciscomasera1994@gmail.com>
2023-01-11 11:52:56 -0300

    unused imports

--- concurrency/src/test/java/org/gl/franciscomasera/concurrency/controller/TaskControllerTest.java
+++ concurrency/src/test/java/org/gl/franciscomasera/concurrency/controller/TaskControllerTest.java
@@ -1,18 +1,12 @@
 package org.gl.franciscomasera.concurrency.controller;
 
-import org.jsmart.zerocode.core.domain.LoadWith;
-import org.jsmart.zerocode.core.domain.TargetEnv;
-import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
