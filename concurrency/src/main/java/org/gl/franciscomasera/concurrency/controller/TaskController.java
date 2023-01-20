9369f491aa160ec4f4737fa435be89dc3a5e7017
Francisco Masera <francisco.masera@externos.bci.cl>
2023-01-20 16:10:29 -0300

    change main

--- concurrency/src/main/java/org/gl/franciscomasera/concurrency/controller/TaskController.java
+++ concurrency/src/main/java/org/gl/franciscomasera/concurrency/controller/TaskController.java
@@ -2,47 +2,28 @@ package org.gl.franciscomasera.concurrency.controller;
 
 import org.gl.franciscomasera.concurrency.domain.MyTask;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController {
 
-    @PostMapping("/race/{poolSize}")
-    public ResponseEntity<String> taskRace(@PathVariable int poolSize) {
+    @PostMapping("/race")
+    public ResponseEntity<String> taskRace() throws InterruptedException {
 
-        System.out.println("Beginning program");
-        final ExecutorService executor = Executors.newFixedThreadPool(poolSize);
-        final ExecutorCompletionService<MyTask> completionService = new ExecutorCompletionService<>(executor);
+        var myTask = new MyTask();
 
-        System.out.println("Loading tasks");
-        for (int i = 1; i < poolSize + 1; i++) {
-            final int delay = (int) ((Math.random() * (poolSize - 1) + 1));
-            final MyTask task = new MyTask(i, delay);
-            completionService.submit(task);
-        }
+        var firstTask = new Thread(myTask);
+        var secondTask = new Thread(myTask);
 
-        MyTask task;
-        try {
-            System.out.println("Preparing results...");
-            task = completionService.take().get();
-            System.out.printf("Winner is task n° %d", task.getTaskNumber());
-            executor.shutdownNow();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        firstTask.start();
+        secondTask.start();
+        firstTask.join();
+        secondTask.join();
 
-        String msg = String.format("Winner is task n° %s", task.getTaskNumber());
-        return ResponseEntity.ok(msg);
+        return ResponseEntity.ok("Ok");
     }
 
 }
--- concurrency/src/main/java/org/gl/franciscomasera/concurrency/domain/MyTask.java
+++ concurrency/src/main/java/org/gl/franciscomasera/concurrency/domain/MyTask.java
@@ -1,28 +1,18 @@
 package org.gl.franciscomasera.concurrency.domain;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Getter
-@RequiredArgsConstructor
-public class MyTask implements Callable<MyTask> {
-
-    private final int taskNumber;
-    @Getter(AccessLevel.NONE)
-    private final int delay;
-    @Getter(AccessLevel.NONE)
-    private int progress;
+@Slf4j
+public class MyTask implements Runnable {
+    Lock lock = new ReentrantLock();
 
     @Override
-    public MyTask call() throws InterruptedException {
-        Thread.sleep(delay);
-        while (progress < 100) {
-            progress += (Math.random() * 15) + 1;
-        }
-        return this;
+    public void run() {
+        lock.lock();
     }
-
 }
