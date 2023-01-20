27a41826703d38dfb124e879af085ba9440bf6dd
Francisco Masera <francisco.masera@externos.bci.cl>
2023-01-20 16:15:38 -0300

    slff4j

--- concurrency/src/main/java/org/gl/franciscomasera/concurrency/controller/TaskController.java
+++ concurrency/src/main/java/org/gl/franciscomasera/concurrency/controller/TaskController.java
@@ -1,5 +1,6 @@
 package org.gl.franciscomasera.concurrency.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.gl.franciscomasera.concurrency.domain.MyTask;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/task")
+@Slf4j
 public class TaskController {
 
     @PostMapping
@@ -17,7 +19,7 @@ public class TaskController {
 
         var firstTask = new Thread(myTask);
         var secondTask = new Thread(myTask);
-
+        log.info("Init");
         firstTask.start();
         secondTask.start();
         firstTask.join();
