1101fa2f617633babf870be86a56798c25c6b6a0
Dargor94 <franciscomasera1994@gmail.com>
2023-02-12 03:58:12 -0300

    client sec. config

---
+++ oauth2/client/src/main/java/org/gl/franciscomasera/oauth2/client/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.gl.franciscomasera.oauth2.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login.loginPage("/oauth2/authorization/products-client-oidc"))
+                .oauth2Client(withDefaults());
+        return http.build();
+    }
+
+}
